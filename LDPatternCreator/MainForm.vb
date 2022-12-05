16b9b07fb63b5d6dec9fa579f5aa41bb4df4404f
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2022-12-05 18:58:47 +0100

    Fixed issue #25.

--- LDPatternCreator/MainForm.vb
+++ LDPatternCreator/MainForm.vb
@@ -1335,7 +1335,7 @@ newTry:
 
                             BtnPipette.Enabled = View.SelectedTriangles.Count = 1 OrElse (MainState.objectToModify = Modified.Primitive AndAlso View.SelectedTriangles.Count >= 1)
                             If BtnAddTriangle.Checked AndAlso MainState.objectToModify = Modified.Vertex Then
-                                If View.SelectedVertices.Count = 0 AndAlso FastTriangulationIIToolStripMenuItem.Checked Then
+                                If delta < 5 AndAlso View.SelectedVertices.Count = 0 AndAlso FastTriangulationIIToolStripMenuItem.Checked Then
                                     LPCFile.Vertices.Add(New Vertex(Math.Round(getXcoordinate(MouseHelper.getCursorpositionX()) / View.moveSnap) * View.moveSnap, Math.Round(getYcoordinate(MouseHelper.getCursorpositionY()) / View.moveSnap) * View.moveSnap, False))
                                     If Not View.SelectedVertices.Contains(ListHelper.LLast(LPCFile.Vertices)) Then
                                         View.SelectedVertices.Add(ListHelper.LLast(LPCFile.Vertices))
