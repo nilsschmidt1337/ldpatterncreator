e5607c38de3c05d65cdad287c79211169e99e654
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2026-04-17 20:06:22 +0200

    Prepared fix for issue #45.

--- LDPatternCreator/helper/MathHelper.vb
+++ LDPatternCreator/helper/MathHelper.vb
@@ -38,4 +38,104 @@ Public Class MathHelper
         value = Math.Min(value, max)
         Return value
     End Function
+
+    ' Define a regular expression to match the significant digits and exponent
+    Private Shared sciRegexWithOptionalSign As New Text.RegularExpressions.Regex("(\+|-)?\d+\.?\d*e-?\d+")
+
+    Public Shared Function ScientificNotationToDecimal(ByVal value As String) As String
+
+        Dim matchWithSign As Text.RegularExpressions.Match = sciRegexWithOptionalSign.Match(value)
+
+        ' Check if the string matches the scientific notation pattern
+        If Not matchWithSign.Success Then
+            ' Return the original value
+            Return value
+        End If
+
+        ' Extract the significant digits and exponent from the match
+        Dim significantDigits As String
+        Dim significantDigitsWithSign As String = matchWithSign.Groups(0).Value
+
+        Dim sign As String
+
+        If significantDigitsWithSign.StartsWith("-") Then
+            significantDigits = significantDigitsWithSign.Substring(1)
+            sign = "-"
+        ElseIf significantDigitsWithSign.StartsWith("+") Then
+            significantDigits = significantDigitsWithSign.Substring(1)
+            sign = "+"
+        Else
+            significantDigits = significantDigitsWithSign
+            sign = ""
+        End If
+
+        ' Remove the 'e' and extract the exponent
+        Dim expIndex As Integer = significantDigits.IndexOf("e")
+        If expIndex <> -1 Then
+            Dim exponent As String = significantDigits.Substring(expIndex + 1)
+            significantDigits = significantDigits.Substring(0, expIndex)
+
+            ' Convert the significant digits to a decimal by moving the decimal point according to the exponent
+            Dim dotIndex As Integer = significantDigits.IndexOf(".")
+            Dim moveBy As Integer = CInt(exponent)
+
+            ' Remove the decimal point
+            significantDigits = significantDigits.Replace(".", "")
+
+            Dim digitCount As Integer = significantDigits.Length
+
+            ' Append or prepend zeros
+            If moveBy < 0 Then
+                ' Prepend
+                For i As Integer = 0 To Math.Abs(moveBy) - 1
+                    significantDigits = "0" & significantDigits
+                Next
+            ElseIf moveBy > 0 Then
+                ' Append
+                For i As Integer = 0 To moveBy - 1
+                    significantDigits &= "0"
+                Next
+            End If
+
+
+            If dotIndex <> -1 Then
+
+                If moveBy < 0 Then
+                    significantDigits = significantDigits.Substring(0, dotIndex) & "." & significantDigits.Substring(dotIndex)
+
+                    ' Remove leading zeros
+                    While significantDigits.StartsWith("0")
+                        significantDigits = significantDigits.Substring(1)
+                    End While
+                ElseIf moveBy > 0 Then
+                    significantDigits = significantDigits.Substring(0, dotIndex + moveBy) & "." & significantDigits.Substring(dotIndex + moveBy)
+
+                    ' Trailing leading zeros
+                    While significantDigits.EndsWith("0")
+                        significantDigits = significantDigits.Substring(0, significantDigits.Length - 1)
+                    End While
+
+                    If significantDigits.EndsWith(".") Then
+                        significantDigits = significantDigits.Substring(0, significantDigits.Length - 1)
+                    End If
+                End If
+
+
+            Else
+                ' No decimal point, just add one at the appropriate position for the exponent value
+                If moveBy < 0 Then
+                    significantDigits = significantDigits.Substring(0, digitCount - moveBy - 1) & "." & significantDigits.Substring(digitCount - moveBy - 1)
+
+                    ' Remove leading zeros
+                    While significantDigits.StartsWith("0")
+                        significantDigits = significantDigits.Substring(1)
+                    End While
+                End If
+            End If
+        End If
+
+        ' Return the formatted number, preserving any sign from the original string
+        Dim finalNumber As String = sign & significantDigits
+        Return finalNumber
+    End Function
 End Class
---
+++ LDPatternCreator/tests/TestSciNotationToDecimal.vb
@@ -0,0 +1,169 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Namespace MathTests
+
+    <TestClass()>
+    Public Class TestSciNotationToDecimal
+        <TestInitialize()>
+        Public Sub Initialize()
+
+        End Sub
+
+        <TestMethod()>
+        Public Sub WrongScientificNotationToDecimal()
+
+            Dim result As String
+            Dim sciNotationA As String = "abc"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("abc", result)
+        End Sub
+
+
+        <TestMethod()>
+        Public Sub SignedScientificNotationToDecimalWithPlus()
+
+            Dim result As String
+            Dim sciNotationA As String = "+1e0"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("+1", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub SignedScientificNotationToDecimalWithMinus()
+
+            Dim result As String
+            Dim sciNotationA As String = "-1e0"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("-1", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub SignedScientificNotationToDecimalWithPlusAndExponent()
+
+            Dim result As String
+            Dim sciNotationA As String = "+1e1"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("+10", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub SignedScientificNotationToDecimalWithPlusAndExponent2()
+
+            Dim result As String
+            Dim sciNotationA As String = "+12e1"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("+120", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub SignedScientificNotationToDecimalWithMinusAndExponent()
+
+            Dim result As String
+            Dim sciNotationA As String = "-1e-1"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("-.1", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub SignedScientificNotationToDecimalWithMinusAndExponent2()
+
+            Dim result As String
+            Dim sciNotationA As String = "-12e-1"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("-1.2", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub SignedScientificNotationToDecimalWithMinusAndExponent3()
+
+            Dim result As String
+            Dim sciNotationA As String = "-123e-1"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("-12.3", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub ScientificNotationToDecimal()
+
+            Dim result As String
+            Dim sciNotationA As String = "1.23e-1"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual(".123", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub ScientificNotationToDecimal2()
+
+            Dim result As String
+            Dim sciNotationA As String = "12.3e-1"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("1.23", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub ScientificNotationToDecimal3()
+
+            Dim result As String
+            Dim sciNotationA As String = "0.123e-1"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual(".0123", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub ScientificNotationToDecimal4()
+
+            Dim result As String
+            Dim sciNotationA As String = "12.3e2"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("1230", result)
+        End Sub
+
+        <TestMethod()>
+        Public Sub ScientificNotationToDecimalComplicated()
+
+            Dim result As String
+            Dim sciNotationA As String = "-1422.33521e-10"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("-.000000142233521", result)
+        End Sub
+
+
+        <TestMethod()>
+        Public Sub ScientificNotationToDecimalComplicated2()
+
+            Dim result As String
+            Dim sciNotationA As String = "1422.33521e4"
+
+            result = MathHelper.ScientificNotationToDecimal(sciNotationA)
+
+            Assert.AreEqual("14223352.1", result)
+        End Sub
+    End Class
+End Namespace
+
